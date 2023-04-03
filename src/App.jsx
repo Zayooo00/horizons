a5e3edd9df24438b1e2eb178e582106a561940a2
Zayooo00 <mrcold135@gmail.com>
2023-04-03 11:02:02 +0200

    Added landing page and basic routing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Start from './pages/Start';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Start.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../theme/css/Start.css';
+
+export default function Start() {
+  return (
+    <div className="flex-mh-box">
+      <div
+        className="img-section overlay-dark-bg-1"
+        style={{
+          backgroundImage:
+            'url(https://24pm.com/images/2023/01/24/midjourney-ultra_realistic_cinematic_photographyfine_artabstrac-mini.jpg)',
+        }}
+      >
+      </div>
+      <div className="img-section-cover"></div>
+      <div className="flex-mh-inner dark-bg-1">
+        <div className="container p-tb flex-container responsive">
+          <div className="six-columns six-offset">
+            <div className="ml-40">
+              <h2 className="large-title-bold">
+                <span className="line1">Welcome to</span>
+                
+                <div className="text-line1-cover"></div>
+                <br />
+                <span className="line2">Horizons</span>
+                <br />
+                <div className="text-line2-cover"></div>
+              </h2>
+              <p className="p-style-bold-up th-20 d-flex-wrap">
+                <span className="line3">
+                  Start generating amazing AI images straight away
+                </span>
+                <div className="text-line3-cover"></div>
+              </p>
+              <div className="mt-30">
+                <div className="border-btn-box border-btn-red pointer-large">
+                  <div className="border-btn-inner">
+                    <a href="/login" className="border-btn" data-text="Try now">
+                      Try now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
