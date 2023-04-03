02ddec979651bc2e37ff23f14b658bb8faaefa78
Zayooo00 <92038816+Zayooo00@users.noreply.github.com>
2023-04-03 21:53:51 +0200

    #14 #15 #18 #19 add google sign in (#20)
    * added login/signup backgrounds locally
    
    * added .env to gitignore
    
    * installed firebase
    
    * configured firebase
    
    * created auth context
    
    * created protected routes
    
    * implemented email and google sign in

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,66 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import theme from './theme/css/theme';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRouteRedirect from './components/ProtectedRouteRedirect';
+import ProtectedRoute from './components/ProtectedRoute';
 
 import Start from './pages/Start';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Create from './pages/Create';
 
 function App() {
+
   return (
     <ChakraProvider theme={theme}>
       <Box bg={theme.palette.primary}>
         <Router>
-          <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-          </Routes>
+          <AuthContextProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRouteRedirect>
+                    <Start />
+                  </ProtectedRouteRedirect>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <ProtectedRouteRedirect>
+                    <Login />
+                  </ProtectedRouteRedirect>
+                }
+              />
+              <Route
+                path="/sign-up"
+                element={
+                  <ProtectedRouteRedirect>
+                    <SignUp />
+                  </ProtectedRouteRedirect>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+                <Route
+                path="/create"
+                element={
+                  <ProtectedRoute>
+                    <Create/>
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </AuthContextProvider>
         </Router>
       </Box>
     </ChakraProvider>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,12 +10,48 @@ import {
   Divider,
   Center,
   Text,
+  Spinner,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import { FcGoogle } from 'react-icons/fc';
+import login from '../assets/images/login.png';
+import { UserAuth } from '../context/AuthContext';
 
 export default function Login() {
+  const { signIn, googleSignIn } = UserAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [authError, setAuthError] = useState('');
+  const [isAuthLoading, setIsAuthLoading] = useState(false);
+
+  const ERROR_INVALID_EMAIL = 'auth/invalid-email';
+  const ERROR_WRONG_PASSWORD = 'auth/wrong-password';
+
+  const handleGoogleSignIn = async () => {
+    await googleSignIn();
+  };
+
+  const handleEmailSignIn = async (event) => {
+    event.preventDefault();
+    setIsAuthLoading(true);
+    setAuthError('');
+    try {
+      await signIn(email, password);
+      setIsAuthLoading(false);
+    } catch (e) {
+      setAuthError(e.message);
+      const errorCode = e.code;
+      if (
+        errorCode === ERROR_INVALID_EMAIL ||
+        errorCode === ERROR_WRONG_PASSWORD
+      ) {
+        setAuthError('The email address or password you entered is invalid.');
+      }
+      setIsAuthLoading(false);
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -29,38 +65,61 @@ export default function Login() {
         >
           <Stack spacing={4} w={'full'} maxW={'md'}>
             <Heading fontSize={'2xl'}>Log in to your account</Heading>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={6}>
-              <Button
-                bg={'#D4E45D'}
-                _hover={{
-                  bg: '#9aaa1d',
-                }}
-                color={'black'}
-                variant={'solid'}
-              >
-                Log in
-              </Button>
-            </Stack>
-            <Stack>
-              <Center>
-                <Divider w={'25%'} />
-                <Text m={2}>Or continue with:</Text>
-                <Divider w={'25%'} />
-              </Center>
-              <Center>
-                <Button rounded={'2rem'} w={'full'} bg={'white'}>
-                  <FcGoogle color="white" alt="Google Icon" />
+            {authError && (
+              <Text fontSize="sm" color="red" mt={8}>
+                {authError}
+              </Text>
+            )}
+            <form onSubmit={handleEmailSignIn}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </FormControl>
+              <Stack spacing={6}>
+                <Button
+                  mt={4}
+                  type="submit"
+                  bg={'#D4E45D'}
+                  _hover={{
+                    bg: '#9aaa1d',
+                  }}
+                  color={'black'}
+                  variant={'solid'}
+                >
+          {isAuthLoading ? <Spinner size={'sm'} /> : 'Log in'}
                 </Button>
-              </Center>
-            </Stack>
+              </Stack>
+              <Stack>
+                <Center>
+                  <Divider w={'25%'} />
+                  <Text m={2}>Or continue with:</Text>
+                  <Divider w={'25%'} />
+                </Center>
+                <Center>
+                  <Button
+                    type="button"
+                    onClick={handleGoogleSignIn}
+                    rounded={'2rem'}
+                    w={'full'}
+                    bg={'white'}
+                  >
+                    <FcGoogle color="white" alt="Google Icon" />
+                  </Button>
+                </Center>
+              </Stack>
+            </form>
           </Stack>
         </Flex>
         <Flex flex={1}>
@@ -68,9 +127,7 @@ export default function Login() {
             display={{ base: 'none', md: 'inline-flex' }}
             alt={'Login Image'}
             objectFit={'cover'}
-            src={
-              'https://lh6.googleusercontent.com/proxy/LrtEOfsjg3iy5G87gQStC1jKbdk7Klxq9p_M3VLVY1D1c_3QdYOAzUl61Qys3DBY02DRosmBTQSuK_9SKdBmOrxiftmOzRo-oMsrwo_IRokLHSLhesctz1f11HX0AlFHTPcJUyRtM-fu=w1920-h937'
-            }
+            src={login}
           />
         </Flex>
       </Stack>
