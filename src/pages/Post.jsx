0d7eaa0b4305872d159115870f27405bde19dc47
Zayooo00 <mrcold135@gmail.com>
2023-05-16 12:22:26 +0200

    created postdetails page

---
+++ src/pages/Post.jsx
@@ -0,0 +1,11 @@
+import PostDetails from '../components/Post/PostDetails';
+import Headbar from '../components/Headbar';
+
+export default function Post() {
+  return (
+    <>
+      <Headbar />
+      <PostDetails />
+    </>
+  );
+}
