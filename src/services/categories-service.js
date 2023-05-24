43c9475a484362f438515dca135dd87ef0d391a9
Zayooo00 <92038816+Zayooo00@users.noreply.github.com>
2023-05-24 11:50:14 +0200

    created get category by name service

--- src/services/categories-service.js
+++ src/services/categories-service.js
@@ -1,5 +1,7 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, where, query } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
+
+import { revertFormatLinkPath } from '../helpers/Normalizer';
 
 const categoriesCollection = collection(db, 'categories');
 
@@ -9,5 +11,19 @@ export async function getAllCategories() {
   querySnapshot.forEach((doc) => {
     categories.push(doc.data());
   });
+
   return categories;
+}
+
+export async function getCategoryByName(categoryName) {
+  const revertedCategory = revertFormatLinkPath(categoryName);
+
+  const q = query(
+    categoriesCollection,
+    where('categoryName', '==', revertedCategory)
+  );
+  const querySnapshot = await getDocs(q);
+  const category = querySnapshot.docs[0]?.data();
+
+  return category;
 }
