5b98cdb18abac0fb1e784adcdc7da9f8d9bec605
Zayooo00 <92038816+Zayooo00@users.noreply.github.com>
2023-05-24 11:50:37 +0200

    implemented path normalizer

--- src/components/Discover/DiscoverCategories.jsx
+++ src/components/Discover/DiscoverCategories.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Box, Grid, GridItem, Image, Text, Stack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 import { getAllCategories } from '../../services/categories-service';
 import categories_bg from '../../assets/images/categories_bg.jpg';
+import { formatLinkPath } from '../../helpers/Normalizer';
 
-export default function CategoryGrid() {
+export default function DiscoverCategories() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
@@ -59,40 +61,59 @@ export default function CategoryGrid() {
       >
         {categories.map((category) => (
           <GridItem key={category.categoryName}>
-            <Box position="relative">
-              <Image
-                src={category.cover}
-                alt={category.categoryName}
-                objectFit="cover"
-                w="50dvw"
-                h="30dvw"
-                maxH={{ base: 150, md: 175, lg: 200, xl: 250 }}
-              />
+            <Link to={`/category/${formatLinkPath(category.categoryName)}`}>
               <Box
-                position="absolute"
-                top="0"
-                left="0"
-                w="100%"
-                h="100%"
-                bg="rgba(0,0,0,0.2)"
-                color="white"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
+                position="relative"
+                _before={{
+                  content: '""',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  bg: 'rgba(0,0,0,0)',
+                  transition: 'background-color 0.2s',
+                }}
+                _hover={{
+                  _before: {
+                    bg: 'rgba(0,0,0,0.5)',
+                  },
+                }}
               >
-                <Text
-                  fontSize={{
-                    base: 'lg',
-                    sm: 'xl',
-                    md: 'xl',
-                    lg: 'xl',
-                    xl: '2xl',
-                  }}
+                <Image
+                  src={category.cover}
+                  alt={category.categoryName}
+                  objectFit="cover"
+                  w="50dvw"
+                  h="30dvw"
+                  maxH={{ base: 150, md: 175, lg: 200, xl: 250 }}
+                />
+                <Box
+                  position="absolute"
+                  top="0"
+                  left="0"
+                  w="100%"
+                  h="100%"
+                  bg="rgba(0,0,0,0.5)"
+                  color="white"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
                 >
-                  {category.categoryName}
-                </Text>
+                  <Text
+                    fontSize={{
+                      base: 'lg',
+                      sm: 'xl',
+                      md: 'xl',
+                      lg: 'xl',
+                      xl: '2xl',
+                    }}
+                  >
+                    {category.categoryName}
+                  </Text>
+                </Box>
               </Box>
-            </Box>
+            </Link>
           </GridItem>
         ))}
       </Grid>
